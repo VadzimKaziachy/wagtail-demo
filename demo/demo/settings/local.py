4b852d561958dd152bc90ed20bb16c034c2a68ae
Vadim Koziachiy <vadim.koziachiy@exposit.com>
2019-05-08 14:43:39 +0300

    update start project

--- demo/demo/settings/local.py
+++ demo/demo/settings/local.py
@@ -7,10 +7,9 @@ DEBUG = True
 SECRET_KEY = '2w=%h$9rvc7&gn+iba6psu28x_(vka+-g1yo3nhll2zj%qswjf'
 
 # SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = ['*'] 
+ALLOWED_HOSTS = ['*']
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
--- demo/demo/settings/production.py
+++ demo/demo/settings/production.py
@@ -2,7 +2,12 @@ from .base import *
 
 DEBUG = False
 
-try:
-    from .local import *
-except ImportError:
-    pass
+# Database
+# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
