655df445a04ebfc9e771d60e4d292c9a71068048
Vadim Koziachiy <vadim.koziachiy@exposit.com>
2019-05-09 17:23:24 +0300

    added Country in block events

--- demo/events/models.py
+++ demo/events/models.py
@@ -4,6 +4,7 @@ from django.utils.translation import ugettext_lazy as _
 from wagtail.admin.edit_handlers import MultiFieldPanel, FieldPanel, InlinePanel
 from wagtail.core.models import Page, Orderable
 from wagtail.snippets.edit_handlers import SnippetChooserPanel
+from wagtail.snippets.models import register_snippet
 
 from modelcluster.models import ClusterableModel
 from modelcluster.fields import ParentalKey
@@ -17,6 +18,9 @@ class City(ClusterableModel):
             FieldPanel('city'),
         ])
     ]
+
+    def __str__(self):
+        return self.city
 
     class Meta:
         verbose_name = _('City')
@@ -35,12 +39,41 @@ class CityRelationship(Orderable, models.Model):
     ]
 
 
+@register_snippet
+class Country(models.Model):
+    """
+    Source: https://wagtail-autocomplete.readthedocs.io/en/latest/using_other_models.html
+    """
+
+    country = models.TextField(verbose_name=_('Country'))
+
+    panels = [
+        MultiFieldPanel([
+            FieldPanel('country'),
+        ])
+    ]
+
+    def __str__(self):
+        return self.country
+
+    class Meta:
+        verbose_name = _('Country')
+
+
 class EventsPage(Page):
+    country = models.ForeignKey(
+        Country,
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+    )
+
     content_panels = [
         MultiFieldPanel(Page.content_panels, heading=_('Page title')),
         InlinePanel('city_relationship',
                     panels=None,
                     label=_('city'),
                     min_num=1,
-                    max_num=3)
+                    max_num=3),
+        SnippetChooserPanel('country'),
     ]
